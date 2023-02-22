30f906a59cc87c1c7a01fa1d15953221a7aa2029
muna-amir <maimuamir@gmail.com>
2023-02-22 09:56:13 -0800

    simple shell

--- builtin1.c
+++ builtin1.c
@@ -70,9 +70,9 @@ int print_alias(list_t *node)
 	if (node)
 	{
 		p = _strchr(node->str, '=');
-		for (a = nod->str; a <= p; a++)
+		for (a = node->str; a <= p; a++)
+			_putchar(*a);
 
-		_putchar(*a);
 		_putchar('\'');
 		_puts(p + 1);
 		_puts("'\n");
